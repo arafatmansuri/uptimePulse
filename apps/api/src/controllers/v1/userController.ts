8bea9706fb4f4db42488fc67ad92bb0eb9de49a0
arafatmansuri <mansuriarafat302@gmail.com>
2026-06-24 15:20:01 +0000

    Add signin contoller

--- apps/api/src/controllers/v1/userController.ts
+++ apps/api/src/controllers/v1/userController.ts
@@ -2,8 +2,8 @@ import { prisma } from "@repo/db";
 import { ApiResponse } from "../../lib/ApiResponse";
 import { AppError } from "../../lib/AppError";
 import { asyncHandler } from "../../lib/asyncHandler";
-import { generateToken, hashPassword } from "../../utils/userUtils";
-import { signUpSchema } from "../../validations/userValidtion";
+import { comparePasswords, generateToken, hashPassword } from "../../utils/userUtils";
+import { signInSchema, signUpSchema } from "../../validations/userValidtion";
 
 export const getUsers = asyncHandler(async (req, res) => {
     const users = await prisma.user.findMany();
@@ -36,6 +36,32 @@ export const signUp = asyncHandler(async (req,res) => {
         data: { refreshToken }
     });
     ApiResponse.success(res, { user: newUser }, "User created successfully", 201,[
+        {name:"access_token", value:accessToken, options:{httpOnly:true, secure:true, sameSite:"none", maxAge: 3600000, path:"/"}},
+        {name:"refresh_token", value:refreshToken, options:{httpOnly:true, secure:true, sameSite:"none", maxAge: 3600000, path:"/"}}
+    ]);
+});
+export const signIn = asyncHandler(async (req,res) => {
+   const parsedSignInData = signInSchema.safeParse(req.body);
+   
+   if (!parsedSignInData.success) {
+    throw AppError.badRequest(parsedSignInData.error.issues[0].message || "Invalid input", { field: parsedSignInData.error.issues[0].path.join(".") });
+   }
+   const { email, password } = parsedSignInData.data;
+    const user = await prisma.user.findFirst({where:{email}});
+    if (!user) {
+        throw AppError.badRequest("User not found", { field: ["email"] });
+    }
+    const isPasswordValid = await comparePasswords(password, user.password);
+    if (!isPasswordValid) {
+        throw AppError.badRequest("Invalid password", { field: ["password"] });
+    }
+    const accessToken = generateToken(user.id, 'access');
+    const refreshToken = generateToken(user.id, 'refresh');
+    await prisma.user.update({
+        where: { id: user.id },
+        data: { refreshToken }
+    });
+    ApiResponse.success(res, { user: user }, "User signed in successfully", 200,[
         {name:"access_token", value:accessToken, options:{httpOnly:true, secure:true, sameSite:"none", maxAge: 3600000, path:"/"}},
         {name:"refresh_token", value:refreshToken, options:{httpOnly:true, secure:true, sameSite:"none", maxAge: 3600000, path:"/"}}
     ]);
