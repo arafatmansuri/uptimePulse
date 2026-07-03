54596cf915a5a85dbe0fc7c9563ab382f4628061
arafatmansuri <mansuriarafat302@gmail.com>
2026-07-03 11:58:12 +0000

    Promosify trackWebsites

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -15,42 +15,40 @@ type GroupMessage = {
     }
 }
 
+async function trackWebsites(websiteToTrack: GroupMessage) {
+   return new Promise<websiteTickCreateManyInput>(async (resolve, reject) => {
+    const startTime = Date.now();
+    await axios.get(websiteToTrack.message.url, {timeout: 5000}).then(() => {
+        resolve({
+            websiteId: websiteToTrack.message.id,
+            response_time_ms: Date.now() - startTime,
+            regionId: "india",
+            status:"UP"
+        });
+    }).catch(() => {
+        resolve({
+            websiteId: websiteToTrack.message.id,
+            response_time_ms: Date.now() - startTime,
+            regionId: "india",
+            status:"DOWN"
+        });
+    })
+   })
+}
+
 const main = async () => {
     while(1){
     const res = await xReadGroup(CONSUMER_GROUP,WORKER_ID,{COUNT:5});
     if(!res || res.length === 0){
         console.log("No new messages");
-        await new Promise(resolve => setTimeout(resolve, 1000*60));
+        await new Promise(resolve => setTimeout(resolve, 1000*5));
         continue;
     }
     let websitesToTrack: GroupMessage[] = res[0]?.messages;
-    const trackedWebsites:websiteTickCreateManyInput[] = await trackWebsites(websitesToTrack);
+    const trackedWebsites:websiteTickCreateManyInput[] = await Promise.all(websitesToTrack.map(trackWebsites));
     await prisma.websiteTick.createMany({data: trackedWebsites});
     await xAck(CONSUMER_GROUP,websitesToTrack.map(w => w.id));
 }
-}
-
-async function trackWebsites(websitesToTrack: GroupMessage[]) {
-   const trackedWebsites:websiteTickCreateManyInput[] = [];
-    websitesToTrack.forEach(async (website) => {
-        let startTime = Date.now();
-        axios.get(website.message.url).then(async () => {
-            trackedWebsites.push({
-                status: "UP",
-                response_time_ms: Date.now() - startTime,
-                websiteId: website.message.id,
-                regionId: CONSUMER_GROUP,
-            });
-        }).catch(async () => {
-            trackedWebsites.push({
-                status: "DOWN",
-                response_time_ms: Date.now() - startTime,
-                websiteId: website.message.id,
-                regionId: CONSUMER_GROUP,
-            });
-        })
-    }) 
-    return trackedWebsites;
 }
 
 main();
