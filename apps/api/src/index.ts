93d6855fdd9e1a3501cd3174123781c7374dcbc0
arafatmansuri <mansuriarafat302@gmail.com>
2026-07-05 15:37:23 +0000

    Remove uniqueness constraint from name

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,11 +3,28 @@ import dotenv from 'dotenv';
 import express from 'express';
 import { globalErrorHandler } from './lib/ErrorHandler';
 import v1Router from './routes/v1';
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:3001",
+  "http://127.0.0.1:3001",
+];
 app.use(express.json());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(<string>origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Allow cookies
+  }),
+);
 app.use(cookieParser());
 
 app.use('/api/v1', v1Router);
